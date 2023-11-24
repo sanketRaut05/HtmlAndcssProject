a995a69d0f6796bbc2a184d6f04962efe6be0690
sanketRaut05 <sanketRautips@email.com>
2023-11-24 17:23:19 +0530

    update

---
+++ project11/css/main.js
@@ -0,0 +1,7 @@
+var element;
+let i=document.getElementById('sanket');
+i.addEventListener('click',function(){
+    document.getElementById('sanket').classList.toggle('ps');
+    var p=document.getElementById('sanket').classList 
+    console.log(p);
+});
